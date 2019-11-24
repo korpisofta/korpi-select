36b62080cb09f1381ae65d8cbe5ae0697ac0cd76
Antti Ahola <antti.ahola@korpisofta.fi>
2019-11-24 13:30:30 +0200

    Set name and version

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject korpi-select "0.1.0"
-  :description "A collection of clojure functions by Korpisofta"
+(defproject org.clojars.korpisofta/korpi-select "1.0.0"
+  :description "XPath -like selection library for clojure maps"
   :url "https://github.com/korpisofta/korpi-select"
   :license {:name "Apache License Version 2.0"
             :url "http://www.apache.org/licenses/"}
